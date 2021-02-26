adcaa411b880443056413eb715dbe09f6b51f775
Yonathan Probeh <Probeh@outlook.com>
2021-02-26 15:57:24 +0200

    Project Update

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,1 +1,1 @@
-codedoc & snapshot & cd ../ & git add . & git commit -m "Project Update" & git push -u origin main & serve & exit
+codedoc & snapshot & cd ../ & git add . & git commit -m "Project Update" & git push -u origin main & ./scripts/serve & exit
