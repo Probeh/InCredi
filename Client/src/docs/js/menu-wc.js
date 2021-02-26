f88eacd65ae9783b6fc93569cbb9238721b097c6
Yonathan Probeh <Probeh@outlook.com>
2021-02-26 16:12:30 +0200

    Project Update

--- Client/src/docs/js/menu-wc.js
+++ Client/src/docs/js/menu-wc.js
@@ -115,9 +115,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
                             <li class="link">
-                                <a href="components/AppComponent.html" data-type="entity-link">AppComponent</a>
-                            </li>
-                            <li class="link">
                                 <a href="components/SearchComponent.html" data-type="entity-link">SearchComponent</a>
                             </li>
                         </ul>
