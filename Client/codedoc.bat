e670a1e9e5eebe52a945c46b6b5e749ada7dc7fd
Yonathan Probeh <Probeh@outlook.com>
2021-02-27 00:02:05 +0200

    Project Update

--- Client/codedoc.bat
+++ Client/codedoc.bat
@@ -1,1 +1,1 @@
-start compodoc -p tsconfig.json -d src/docs -n InCredi ./ --hideGenerator --theme readthedocs & exit
+start compodoc -p tsconfig.json -d src/docs -n InCredi ./ --hideGenerator --theme readthedocs && exit
